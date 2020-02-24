d6d59a0a33b9ba3958a5e495f4d117d0b3980893
Drizii <58788333+Drizii@users.noreply.github.com>
2020-02-24 16:17:45 +0100

    Update views.py

--- choicer/main/views.py
+++ choicer/main/views.py
@@ -7,12 +7,40 @@ from django.views import generic
 
 class Persons_view(generic.ListView):
     model = Person
+    
+# do testowania, widok, gdzie nie ma queryset
+#  https://stackoverflow.com/questions/39012367/how-to-access-many-to-many-field-in-class-based-views-in-django
+def person_view2(request, id)
+    person = get_object_or_404(Person, id=id)
+    pref_of_person = book.question.all()
+    template = 'main/Person_list.html'
+    ceontext = {'pref_of_person' : pref_of_person}
+    return render(request, template, context)
 
-
+#do testowania, ma zwracać liste gier gdzie pref sa takie same dla gry i osoby
+#  https://stackoverflow.com/questions/7223463/django-select-object-from-db-in-a-template
+game_list = []
+for preferences in person.preferences:
+    for preferences in game.preferences:
+        try:
+            best_option = Best_option.objects.get(preferences:preferences, preferences:preferences)
+        except Best_option.DoesNotExist:
+            best_optio = None
+        game_list.append(best_option)
+        
+#  https://django-filter.readthedocs.io/en/master/ref/filters.html
+#  filtry w django
 
 
 
 '''
+def book_detail_view(request, id):
+    book = get_object_or_404(Book, id=id)
+    authors_of_book = book.questions.all()
+    template = 'books/book_detail1.html'
+    context = {'authors_of_book': authors_of_book}
+    return render(request, template, context)
+
 def wybierz_gre(request):
     p_obj = Person.objects.get(id=1)
     kontekst = {
